6b5ff9e99cd4f8de3b7602772a97b3151d408c95
Andrew Larsen <andrew@compozelabs.com>
2023-04-02 06:32:51 +0530

    update deployment environment variables

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -6,12 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 APP_NAME=$1
 VERSION=$2
 REGION=$3
-
-if [ -z "${ENV:-}" ]; then
-    echo "ENV is not set. Please set ENV to the environment you want to deploy to."
-    exit 1
-fi
-
+# NOTE - this always needs to be the last param so it can be passed in on the command line
+ENV=$4
 
 ## declare functions
 . ./scripts/login.sh
@@ -21,7 +17,7 @@ fi
 if [[ -z "${ENV}" ]]; then
     echo "$ENV is not a valid environment, must be either dev, stage, or prod; re-run with npm run deploy <environment>"
     exit 1
-else
+else 
     echo "building docker image"
     "${SCRIPT_DIR}/build_tag.sh" "$APP_NAME" "$VERSION"
     echo "Deploying to $ENV..."
